ec565b52fae1d0e538450cfc01f1e9871bffde65
modrikh <modrikh@users.noreply.github.com>
2026-08-19 11:30:07 +0100

    correction SonarQube python:S1172: middleware en __call__ (plus de parametre inutilise)

--- demoapp/security.py
+++ demoapp/security.py
@@ -1,10 +1,12 @@
 """Middleware minimaliste de securite (CSP, Permissions-Policy, cache)."""
 
-from django.utils.deprecation import MiddlewareMixin
 
+class SecurityHeadersMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
 
-class SecurityHeadersMiddleware(MiddlewareMixin):
-    def process_response(self, request, response):
+    def __call__(self, request):
+        response = self.get_response(request)
         response["Content-Security-Policy"] = (
             "default-src 'self'; style-src 'self' 'unsafe-inline'; "
             "script-src 'self'; img-src 'self' data:; frame-ancestors 'none'"
